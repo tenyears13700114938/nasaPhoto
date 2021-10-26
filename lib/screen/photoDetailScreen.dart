6c9c6ad0f994ee46b0541795cb1bb124ff161bf9
sun tianhai <tenyears13700114938@gmail.com>
2021-10-26 16:32:06 +0900

    bookmark button add

--- lib/screen/photoDetailScreen.dart
+++ lib/screen/photoDetailScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_nasa_photo/model/nasaPhoto.dart';
@@ -6,6 +5,7 @@ import 'package:flutter_nasa_photo/view/photoItemView.dart';
 
 class PhotoDetailScreen extends StatelessWidget {
   final NasaPhoto nasaPhoto;
+
   PhotoDetailScreen(this.nasaPhoto);
 
   @override
@@ -16,10 +16,12 @@ class PhotoDetailScreen extends StatelessWidget {
         children: [
           ConstrainedBox(
             constraints: BoxConstraints(
-              maxHeight:  MediaQuery.of(context).size.width - MediaQuery.of(context).padding.horizontal,
-              maxWidth:  MediaQuery.of(context).size.width - MediaQuery.of(context).padding.horizontal
-              ),
-            child: PhotoItemView(nasaPhoto.url, nasaPhoto.title, nasaPhoto.date, isShowShadow:true),
+                maxHeight: MediaQuery.of(context).size.width -
+                    MediaQuery.of(context).padding.horizontal,
+                maxWidth: MediaQuery.of(context).size.width -
+                    MediaQuery.of(context).padding.horizontal),
+            child: PhotoItemView(nasaPhoto.url, nasaPhoto.title, nasaPhoto.date,
+                isShowShadow: true),
           ),
           //Expanded(child: PhotoItemView(nasaPhoto.url, nasaPhoto.title, nasaPhoto.date)),
           Padding(
@@ -30,6 +32,24 @@ class PhotoDetailScreen extends StatelessWidget {
                 fontSize: 20,
                 //color: Colors.white
               ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 8),
+            child: SizedBox(
+              width: double.infinity,
+              child: TextButton.icon(
+                  onPressed: null,
+                  icon: Icon(Icons.bookmark),
+                  label: Text("Add Book mark"),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Theme.of(context)
+                        .floatingActionButtonTheme
+                        .foregroundColor),
+                    backgroundColor: MaterialStateProperty.all(Theme.of(context)
+                        .floatingActionButtonTheme
+                        .backgroundColor),
+                  )),
             ),
           )
         ],
