a41a9f5920bf8d5ce17e6fe1ef8821b1a8ec7aba
sun tianhai <tenyears13700114938@gmail.com>
2021-10-26 10:57:32 +0900

    add android launch icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_nasa_photo/model/nasaPhotoListModel.dart';
 import 'package:flutter_nasa_photo/repository/dataBaseRepository.dart';
 import 'package:flutter_nasa_photo/repository/nasaRepository.dart';
 import 'package:flutter_nasa_photo/repository/webRepository.dart';
 import 'package:flutter_nasa_photo/view/photoGridView.dart';
 import 'package:provider/provider.dart';
+import 'nasaPhotoTheme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  //SystemChrome.setEnabledSystemUIMode (SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
   runApp(
       ChangeNotifierProvider(
         create: (context) {
@@ -22,22 +25,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final theme = NasaPhotoTheme.dark();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.teal
-      ),
+      theme: theme,
       home: MyHomePage(key: ValueKey("todo"), title: 'Flutter Nasa Photo Page'), //NasaWebView("https://www.youtube.com/embed/aokGqxVdpz0?rel=0"),/
     );
   }
